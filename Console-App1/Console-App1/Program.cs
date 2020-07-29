08a95d93d91064f663f815c3dc2c9eaf054cf0e5
OmeSaranu <peddi.mahalakshmi@gmail.com>
2020-07-29 22:01:13 +0100

    Called featureB

--- Console-App1/Console-App1/Program.cs
+++ Console-App1/Console-App1/Program.cs
@@ -10,6 +10,7 @@ namespace Console_App1
             Console.WriteLine("Made some changes in Github");
             Console.WriteLine("Made some changes in VS");
             Console.WriteLine(FeatureA.add(1,2));
+            Console.WriteLine(FeatureB.Substract(3, 2));
         }
     }
 }
