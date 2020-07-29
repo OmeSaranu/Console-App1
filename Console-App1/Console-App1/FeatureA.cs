788f7b2b4ae31e1fb786bfe9bf1320f54f32f71e
OmeSaranu <peddi.mahalakshmi@gmail.com>
2020-07-29 21:29:31 +0100

    completed featureA

--- Console-App1/Console-App1/FeatureA.cs
+++ Console-App1/Console-App1/FeatureA.cs
@@ -6,5 +6,12 @@ namespace Console_App1
 {
     class FeatureA
     {
+        public static int add(int x1, int x2)
+        { 
+          int result = x1 + x2; 
+          return result;
+        }
+      
+
     }
 }
